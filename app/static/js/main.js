f8415b0ad92176bfa300cb42716bbcc395c7073b
TIENLAM <lamluong>
2024-03-28 22:58:39 +0700

    [FE] Add patient search view

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -21,6 +21,33 @@ $("#logout").click(function() {
   function(data, status) {
     window.location.replace(window.location.origin)
   }).fail(function(){
-    alert("ngu")
+    alert("Invalid token")
+  });
+});
+
+$("#search-patient").click(function() {
+  var url = window.location.origin + '/get-patient'
+  var doc_id = $('#search-with-id').val()
+
+  if (!doc_id) {
+    alert("Trường tìm kiếm không được để trống")
+    return false;
+  }
+  $.get(url, { document_id :  doc_id},
+  function(data, status) {
+    console.log(data.mother_name)
+    $('#not-found-search-result').hide();
+    $('#child-name').text(data.name)
+    $('#mother-name').text(data.mother_name)
+    $('#mother-phone').text(data.phone)
+    
+    doc_url = window.location.origin + '/patient-doc/?document_name=' + data.document_path
+    $('#document-name-url').attr("href", doc_url);
+
+    $("#search-result").css("visibility", "visible");
+
+  }).fail(function(){
+    $("#search-result").css("visibility", "hidden");
+    $('#not-found-search-result').show();
   });
 });
